de3f0edd14dcd91bbf72c65d7b24a6ea6228f0ec
Diego Aaron <daavv2603@gmail.com>
2020-08-17 21:19:22 -0500

    otro push

--- dist/index.js
+++ dist/index.js
@@ -7,10 +7,7 @@ const server_1 = __importDefault(require("./clases/server"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const express_fileupload_1 = __importDefault(require("express-fileupload"));
-const Post_1 = __importDefault(require("./Rutas/Post"));
-const cors_1 = __importDefault(require("cors"));
-const usuario_model_1 = require("./models/usuario.model");
-const token_1 = __importDefault(require("./clases/token"));
+const Usuario_1 = __importDefault(require("./Rutas/Usuario"));
 const server = new server_1.default();
 //BodyParser
 server.app.use(body_parser_1.default.urlencoded({ extended: true }));
@@ -26,49 +23,16 @@ server.app.use(express_fileupload_1.default());
 // optionsSuccessStatus: 204 } 
 // server.app.options('*',cors(corsOptions));
 // server.app.use(cors(corsOptions));
-//rutas de mi aplicacion
-//server.app.use( '/user', userRoutes )
-//Login
-var corsOptions = { origin: 'http://localhost:8100',
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-API-KEY', 'Origin', 'X-Requested-With', 'Accept', 'Access-Control-Allow-Request-Method', 'x-token'],
-    preflightContinue: false,
-    optionsSuccessStatus: 204 };
-server.app.options('/login', cors_1.default(corsOptions));
-server.app.post('/login', cors_1.default(), (req, resp) => {
-    const body = req.body;
-    usuario_model_1.Usuario.findOne({ email: body.email }, (err, userDB) => {
-        if (err) {
-            throw err;
-        }
-        ;
-        if (!userDB) {
-            return resp.json({
-                ok: false,
-                mensaje: 'Usuario/Contraseña son incorrectos'
-            });
-        }
-        if (userDB.compararPassword(body.password)) {
-            const tokenUser = token_1.default.getJwtToken({
-                _id: userDB._id,
-                nombre: userDB.nombre,
-                email: userDB.email,
-                avatar: userDB.avatar
-            });
-            resp.json({
-                ok: true,
-                token: tokenUser
-            });
-        }
-        else {
-            return resp.json({
-                ok: false,
-                mensaje: 'Usuario/Contraseña son incorrectos****'
-            });
-        }
-    });
+server.app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
 });
-server.app.use('/posts', Post_1.default);
+//rutas de mi aplicacion
+server.app.use('/user', Usuario_1.default);
+//Login
 //conectar BD
 mongoose_1.default.connect('mongodb+srv://SweptAaron:CyD160412@cluster0-ok4he.mongodb.net/fotosgram?retryWrites=true&w=majority', { useNewUrlParser: true,
     useCreateIndex: true,
