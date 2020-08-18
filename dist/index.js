783aebc3a037161664ed5b74275c578e5b677263
Diego Aaron <daavv2603@gmail.com>
2020-08-17 19:40:36 -0500

    otro push

--- dist/index.js
+++ dist/index.js
@@ -17,11 +17,11 @@ server.app.use(body_parser_1.default.json());
 //FileUpload
 server.app.use(express_fileupload_1.default());
 //allow cross config
-server.app.use(cors_1.default({ origin: true, credentials: true }));
+//server.app.use( cors({ origin: true, credentials: true }) );
 server.app.use(cors_1.default());
 server.app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-COntrol-Allow-Request-Method, x-token');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, x-token');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
