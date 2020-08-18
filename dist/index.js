1865a62072d25468d56129071b35d78947aa314d
Diego Aaron <daavv2603@gmail.com>
2020-08-18 17:46:08 -0500

    cors content type

--- dist/index.js
+++ dist/index.js
@@ -23,6 +23,7 @@ server.app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, x-token');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Content-Type', 'application/json; charset=utf-8');
     next();
 });
 //rutas de mi aplicacion
