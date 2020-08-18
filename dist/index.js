dc33021e4c46a3cdd0832e3225d9ad1c030f96c0
Diego Aaron <daavv2603@gmail.com>
2020-08-17 19:12:26 -0500

    otro push

--- dist/index.js
+++ dist/index.js
@@ -24,28 +24,28 @@ var corsOptionsDelegate = function (req, callback) {
     if (whitelist.indexOf(req.header('Origin')) !== -1) {
         corsOptions = { origin: "*",
             methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-            preflightContinue: false,
+            preflightContinue: true,
             optionsSuccessStatus: 204 }; // reflect (enable) the requested origin in the CORS response
     }
     else {
         corsOptions = { origin: "*",
             methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-            preflightContinue: false,
+            preflightContinue: true,
             optionsSuccessStatus: 204 };
     } // disable CORS for this request
     callback(null, corsOptions); // callback expects two parameters: error and options
 };
 server.app.use(cors_1.default(corsOptionsDelegate));
 server.app.options('*', cors_1.default());
-server.app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// server.app.use(function (req, res, next) {
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request headers you wish to allow
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 console.log();
 //rutas de mi aplicacion
 server.app.use('/user', Usuario_1.default);
