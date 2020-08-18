317e710f31e10079f238cbb6ae9dbbf679e74d99
Diego Aaron <daavv2603@gmail.com>
2020-08-17 21:53:28 -0500

    otro push

--- dist/index.js
+++ dist/index.js
@@ -9,6 +9,9 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const express_fileupload_1 = __importDefault(require("express-fileupload"));
 const Usuario_1 = __importDefault(require("./Rutas/Usuario"));
 const Post_1 = __importDefault(require("./Rutas/Post"));
+const cors_1 = __importDefault(require("cors"));
+const usuario_model_1 = require("./models/usuario.model");
+const token_1 = __importDefault(require("./clases/token"));
 const server = new server_1.default();
 //BodyParser
 server.app.use(body_parser_1.default.urlencoded({ extended: true }));
@@ -17,22 +20,46 @@ server.app.use(body_parser_1.default.json());
 server.app.use(express_fileupload_1.default());
 //allow cross config
 //server.app.use( cors({ origin: true, credentials: true }) );
-// var corsOptions = { origin: true,
-// methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-// allowedHeaders: ['Content-Type', 'Authorization', 'X-API-KEY', 'Origin', 'X-Requested-With', 'Accept', 'Access-Control-Allow-Request-Method', 'x-token'],
-// preflightContinue: true,
-// optionsSuccessStatus: 204 } 
-// server.app.options('*',cors(corsOptions));
-// server.app.use(cors(corsOptions));
-server.app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
 //rutas de mi aplicacion
-server.app.use('/user', Usuario_1.default);
+//server.app.use( '/user', userRoutes )
+var corsOptions = { origin: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-API-KEY', 'Origin', 'X-Requested-With', 'Accept', 'Access-Control-Allow-Request-Method', 'x-token'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204 };
+Usuario_1.default.post('/login', cors_1.default(corsOptions), (req, resp) => {
+    const body = req.body;
+    usuario_model_1.Usuario.findOne({ email: body.email }, (err, userDB) => {
+        if (err) {
+            throw err;
+        }
+        ;
+        if (!userDB) {
+            return resp.json({
+                ok: false,
+                mensaje: 'Usuario/Contraseña son incorrectos'
+            });
+        }
+        if (userDB.compararPassword(body.password)) {
+            const tokenUser = token_1.default.getJwtToken({
+                _id: userDB._id,
+                nombre: userDB.nombre,
+                email: userDB.email,
+                avatar: userDB.avatar
+            });
+            resp.json({
+                ok: true,
+                token: tokenUser
+            });
+        }
+        else {
+            return resp.json({
+                ok: false,
+                mensaje: 'Usuario/Contraseña son incorrectos****'
+            });
+        }
+    });
+});
 server.app.use('/posts', Post_1.default);
 //Login
 //conectar BD
@@ -47,3 +74,10 @@ mongoose_1.default.connect('mongodb+srv://SweptAaron:CyD160412@cluster0-ok4he.mo
 server.start(() => {
     console.log(`servidor corriendo el puerto ${server.port}`);
 });
+// var corsOptions = { origin: true,
+// methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+// allowedHeaders: ['Content-Type', 'Authorization', 'X-API-KEY', 'Origin', 'X-Requested-With', 'Accept', 'Access-Control-Allow-Request-Method', 'x-token'],
+// preflightContinue: true,
+// optionsSuccessStatus: 204 } 
+// server.app.options('*',cors(corsOptions));
+// server.app.use(cors(corsOptions));
