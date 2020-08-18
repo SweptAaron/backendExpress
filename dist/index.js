cac89a6eb91ac60e615653c52ad085c60af0698f
Diego Aaron <daavv2603@gmail.com>
2020-08-17 20:48:04 -0500

    otro push

--- dist/index.js
+++ dist/index.js
@@ -7,8 +7,10 @@ const server_1 = __importDefault(require("./clases/server"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const express_fileupload_1 = __importDefault(require("express-fileupload"));
-const Usuario_1 = __importDefault(require("./Rutas/Usuario"));
 const Post_1 = __importDefault(require("./Rutas/Post"));
+const cors_1 = __importDefault(require("cors"));
+const usuario_model_1 = require("./models/usuario.model");
+const token_1 = __importDefault(require("./clases/token"));
 const server = new server_1.default();
 //BodyParser
 server.app.use(body_parser_1.default.urlencoded({ extended: true }));
@@ -25,7 +27,47 @@ server.app.use(express_fileupload_1.default());
 // server.app.options('*',cors(corsOptions));
 // server.app.use(cors(corsOptions));
 //rutas de mi aplicacion
-server.app.use('/user', Usuario_1.default);
+//server.app.use( '/user', userRoutes )
+//Login
+var corsOptions = { origin: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-API-KEY', 'Origin', 'X-Requested-With', 'Accept', 'Access-Control-Allow-Request-Method', 'x-token'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204 };
+server.app.options('/login', cors_1.default(corsOptions));
+server.app.post('/login', cors_1.default(corsOptions), (req, resp) => {
+    const body = req.body;
+    usuario_model_1.Usuario.findOne({ email: body.email }, (err, userDB) => {
+        if (err) {
+            throw err;
+        }
+        ;
+        if (!userDB) {
+            return resp.json({
+                ok: false,
+                mensaje: 'Usuario/Contraseña son incorrectos'
+            });
+        }
+        if (userDB.compararPassword(body.password)) {
+            const tokenUser = token_1.default.getJwtToken({
+                _id: userDB._id,
+                nombre: userDB.nombre,
+                email: userDB.email,
+                avatar: userDB.avatar
+            });
+            resp.json({
+                ok: true,
+                token: tokenUser
+            });
+        }
+        else {
+            return resp.json({
+                ok: false,
+                mensaje: 'Usuario/Contraseña son incorrectos****'
+            });
+        }
+    });
+});
 server.app.use('/posts', Post_1.default);
 //conectar BD
 mongoose_1.default.connect('mongodb+srv://SweptAaron:CyD160412@cluster0-ok4he.mongodb.net/fotosgram?retryWrites=true&w=majority', { useNewUrlParser: true,
